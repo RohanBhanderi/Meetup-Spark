864d4d9daf52c2a3c913386bc05facae532d71ff
RohanBhanderi <rohan.bhanderi@gmail.com>
2015-12-01 04:53:25 -0800

    Add rerun changes

--- rerun.sh
+++ rerun.sh
@@ -3,7 +3,7 @@
 export SPARK_HOME=/opt/mapr/spark/spark-1.4.1
 
 $SPARK_HOME/bin/spark-submit \
-	--class SyslogSparkStreamPrinter \
+	--class EventComntsStreamProcessor \
 	--master local[2] \
-	/user/$USER/LAB2_SUBMISSION/E1/kafka.jar localhost:5181 default cs286 1 \
-	> /user/user01/syslog2spark.txt
+	/home/rohan/meetup-analysis/event-comms/Meetup-Spark/target/event-comms-spark.jar \
+	localhost:9092 event_comms \
